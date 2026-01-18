cdeee4dff6a86d98ca229511ffdf3f12568d3346
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-18 13:06:26 +0100

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,9 +1,10 @@
-
+#!/bin/sh
 
 # Busca ficheros en el sistema que se ejecuten como Super User
 # Vamos a usar esos ficheros para ejecutar una Shell como Root
 # (r) hackingyseguridad.com 2026
 # antonio_taboada
+# Uso.: $sh root.sh 
 
 echo " busca fichero que se ejecutan como root en el sistema:"
 find / -perm -4000 2>/dev/null
